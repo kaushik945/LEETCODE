4c5a7e87dca18a8a153ec353c29ba5e174a1074b
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-02 13:16:56 +0530

    Time: 7 ms (92.6%), Space: 32.5 MB (49%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,32 +1,45 @@
 class Solution {
+    static constexpr int DIRS[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dis(m, vector<int>(n, -1));
-        int dirs[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        vector<vector<int>> dis(m, vector<int>(n, INT_MAX));
 
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
-                       greater<>>
-            pq;
-        pq.emplace(grid[0][0], 0, 0);
-        while (!pq.empty()) {
-            auto [val, cx, cy] = pq.top();
-            pq.pop();
-            if (dis[cx][cy] >= 0) {
-                continue;
+        deque<pair<int, int>> q;
+        q.emplace_front(0, 0);
+        dis[0][0] = grid[0][0];
+
+        while (!q.empty()) {
+            auto [cx, cy] = q.front();
+            q.pop_front();
+            // the first time it leaves the queue, the shortest distance is
+            // guaranteed
+            if (cx == m - 1 && cy == n - 1) {
+                return true;
             }
-            dis[cx][cy] = val;
-            for (int k = 0; k < 4; k++) {
-                int nx = cx + dirs[k][0];
-                int ny = cy + dirs[k][1];
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
-                    dis[nx][ny] >= 0) {
+
+            for (auto& [dx, dy] : DIRS) {
+                int nx = cx + dx, ny = cy + dy;
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
                     continue;
                 }
-                pq.emplace(val + grid[nx][ny], nx, ny);
+                int cost = dis[cx][cy] + grid[nx][ny];
+                // pruning: the new distance does not meet health requirements
+                if (cost >= health) {
+                    continue;
+                }
+                if (cost < dis[nx][ny]) {
+                    dis[nx][ny] = cost;
+                    if (grid[nx][ny] == 0) {
+                        q.emplace_front(nx, ny);
+                    } else {
+                        q.emplace_back(nx, ny);
+                    }
+                }
             }
         }
 
-        return dis[m - 1][n - 1] < health;
+        return false;
     }
 };
