81f44ee9fe3808575800346aefaaf334e1930a9f
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-09 14:19:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.86%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int square(int num) {
+        int ans = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            ans += rem * rem;
+            num /= 10;
+        }
+
+        return ans;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+};
