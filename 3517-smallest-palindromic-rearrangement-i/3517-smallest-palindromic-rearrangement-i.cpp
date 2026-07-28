4469b76f48c3890403f3fbd2c66729a2f0418c0d
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-28 21:46:30 +0530

    Time: 8 ms (98.9%), Space: 54.4 MB (85.71%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,16 +4,15 @@ public:
         int n = s.size();
         int freq[26] = {0};
         int odd = -1;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n/2; i++){
             freq[s[i]-'a']++;
         }
-        freq[s[n/2]-'a']--; 
         for(int i = 0; i < n/2; i++){
             for(int j = 0; j < 26; j++){
                 if(freq[j]>0){
                     s[i] = j + 'a';
                     s[n-i-1] = j + 'a';
-                    freq[j] -= 2;
+                    freq[j] -= 1;
                     break;
                 }
             }
