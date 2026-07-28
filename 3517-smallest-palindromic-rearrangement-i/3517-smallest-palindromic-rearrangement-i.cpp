a1ffb771f7c1cbcf06a7ebc3ede8269117bc0e77
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-28 21:26:38 +0530

    Time: 54 ms (47.69%), Space: 60.7 MB (81.32%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        string ans = s;
+        vector<int> freq(26,0);
+        int odd = -1;
+        for(int i = 0; i < n; i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i = 0; i < n; i++){
+            if(freq[s[i]-'a']%2) odd = i;
+        }
+        if(n%2){
+            ans[n/2] = s[odd];
+            freq[s[odd]-'a']--;
+        }
+        for(int i = 0; i < n/2; i++){
+            for(int j = 0; j < 26; j++){
+                if(freq[j]>0){
+                    ans[i] = j + 'a';
+                    ans[n-i-1] = j + 'a';
+                    freq[j] -= 2;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
