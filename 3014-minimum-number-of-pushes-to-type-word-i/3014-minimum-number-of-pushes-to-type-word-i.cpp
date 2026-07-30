961e23b952c79f2e7d9eec25d95daf751c026d81
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-30 10:01:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,8 +3,8 @@ public:
     int minimumPushes(string word) {
         int n = word.size();
         int ans = 0;
-       ans+= 8*((n/8)*(n/8+1))/2;
-        ans += (n/8+1)*(n%8);
+        ans += 8 * ((n / 8) * (n / 8 + 1)) / 2;
+        ans += (n / 8 + 1) * (n % 8);
         return ans;
     }
 };
