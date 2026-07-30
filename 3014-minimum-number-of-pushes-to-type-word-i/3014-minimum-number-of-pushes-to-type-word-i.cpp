abe73dc6cfbf8cd352908c76e612bdb60ad80bfa
Kaushik Mishra <57583337+kaushik945@users.noreply.github.com>
2026-07-30 10:01:04 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,9 +3,7 @@ public:
     int minimumPushes(string word) {
         int n = word.size();
         int ans = 0;
-        for(int i = 1; i <= n/8;i++){
-            ans += 8*(i);
-        }
+       ans+= 8*((n/8)*(n/8+1))/2;
         ans += (n/8+1)*(n%8);
         return ans;
     }
